1a527e0e6e9993fa5716f8fb125e76db3c0b7d7f
Felix Berger <bflat1@gmx.net>
2010-07-17 00:45:46 -0400

    works for class files

---
+++ src/main/java/properator/Files.java
@@ -0,0 +1,87 @@
+package properator;
+
+import java.io.File;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+
+public class Files {
+	
+	public static Iterable<File> getFilesRecursive(final File directory, final String...extensions) {
+		return new Iterable<File>() {
+			@Override
+			public Iterator<File> iterator() {
+				return new RecursiveFileIterator(directory, extensions != null ? extensions : new String[0]);
+			}
+		};
+	}
+	
+	private static class RecursiveFileIterator implements Iterator<File> {
+		
+		private final Queue<File> directoriesToSearch = new ArrayDeque<File>();
+		
+		private final Queue<File> foundFiles = new ArrayDeque<File>();
+
+		private final List<String> extensions;
+
+		public RecursiveFileIterator(File directory, String[] extensions) {
+			this.extensions = dotify(extensions);
+			if (directory.isDirectory()) {
+				directoriesToSearch.add(directory);
+			}
+		}
+
+		private List<String> dotify(String[] extensions) {
+			List<String> dotted = new ArrayList<String>(extensions.length);
+			for (String extension : extensions) {
+				dotted.add("." + extension);
+			}
+			return dotted;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return !foundFiles.isEmpty() || (!directoriesToSearch.isEmpty() && findFiles());
+		}
+
+		private boolean findFiles() {
+			while (!directoriesToSearch.isEmpty() && foundFiles.isEmpty()) {
+				File directory = directoriesToSearch.remove();
+				File[] files = directory.listFiles();
+				if (files == null) {
+					continue;
+				}
+				for (File file : files) {
+					if (file.isDirectory()) {
+						directoriesToSearch.add(file);
+					} else if (matchesOneExtension(file)) { 
+						foundFiles.add(file);
+					}
+				}
+			}
+			return !foundFiles.isEmpty();
+		}
+
+		private boolean matchesOneExtension(File file) {
+			String name = file.getName();
+			for (String extension : extensions) {
+				if (name.endsWith(extension)) {
+					return true;
+				}
+			}
+			return extensions.isEmpty();
+		}
+
+		@Override
+		public File next() {
+			return foundFiles.remove();
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	}
+}
---
+++ src/main/java/properator/Log4JPropertiesGenerator.java
@@ -0,0 +1,70 @@
+package properator;
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Set;
+import java.util.TreeSet;
+
+import com.sun.org.apache.bcel.internal.classfile.ClassParser;
+import com.sun.org.apache.bcel.internal.classfile.Field;
+import com.sun.org.apache.bcel.internal.classfile.JavaClass;
+
+public class Log4JPropertiesGenerator {
+	
+	private static final String TEMPLATE = "###\n# log4j configuration file.\n########\n\n#########\n# Valid thresholds can be:\n# OFF, FATAL, ERROR, WARN, INFO, DEBUG, ALL \n\n#\n# The default logger that is used prints out log statements\n# on the console.  If you want those redirected to a file,\n# enable TextFile logger.  If you want those stored in an xml\n# format (either for chainsaw or for inclusing in xml documents)\n# use the XMLFile logger.  If you want to use chainsaw to watch\n# the logs of a running application use the \"socket\" logger\n\n########\n\n# Set the root loggers\nlog4j.rootLogger=OFF, stdout\n\n\n######\n#  The TextFile logger\n# if you want to enable logging to file in standard format:\n# 1. comment out the first line\n# 2. uncomment/edit the other lines\n# to disable this type of logging do the oppposite.\n\n#log4j.appender.TextFile=org.apache.log4j.varia.NullAppender\nlog4j.appender.TextFile=org.apache.log4j.RollingFileAppender\nlog4j.appender.TextFile.File=log.txt\nlog4j.appender.TextFile.MaxFileSize=5000MB\nlog4j.appender.TextFile.MaxBackupIndex=5\nlog4j.appender.TextFile.layout=org.apache.log4j.PatternLayout\nlog4j.appender.TextFile.layout.ConversionPattern=%-6r %-5p [%t] %c{2}.%M - %m%n\nlog4j.appender.TextFile.ImmediateFlush=true\n########\n\n\n######\n# The XMLFile logger\n# if you want to enable logging to XML file for the chainsaw viwer:\n# 1. comment out the first line\n# 2. uncomment/edit the other lines\n# to disable this type of logging do the oppposite.\n\nlog4j.appender.XMLFile=org.apache.log4j.varia.NullAppender\n#log4j.appender.XMLFile=org.apache.log4j.RollingFileAppender\n#log4j.appender.XMLFile.File=log.xml\n#log4j.appender.XMLFile.MaxFileSize=100MB\n#log4j.appender.XMLFile.MaxBackupIndex=5\n#log4j.appender.XMLFile.layout=org.apache.log4j.xml.XMLLayout\n#log4j.appender.XMLFile.layout.LocationInfo=true\n#log4j.appender.XMLFile.ImmediateFlush=false\n#####\n\n\n\n#########\n# The \"socket\" logger\n# If you want to use the Chainsaw viewer on a running program:\n# 1. comment out the first line\n# 2. uncomment/edit the other lines\n# to disable this type of logging do the oppposite.\n\n#log4j.appender.socket=org.apache.log4j.varia.NullAppender\nlog4j.appender.socket=org.apache.log4j.net.SocketAppender\nlog4j.appender.socket.RemoteHost=localhost\nlog4j.appender.socket.port=4445\nlog4j.appender.socket.LocationInfo=true\n#########\n\n# stdout is set to be ConsoleAppender sending its output to System.out\n#log4j.appender.stdout=org.apache.log4j.varia.NullAppender\nlog4j.appender.stdout=org.apache.log4j.ConsoleAppender\n\n# stdout uses PatternLayout.\nlog4j.appender.stdout.layout=org.apache.log4j.PatternLayout\n\n# The conversion pattern is:\n# time elapsed since start of logging (left justified, pads extra spaces if less than 0)\n# logging priority (left justified, pads extra spaces if less than 5)\n# [thread name]\n# packagename.classname.methodname (only the last part of the package is kept)\n# - message\n# newline\n\nlog4j.appender.stdout.layout.ConversionPattern=%-6r %-5p [%t] %c{2}.%M - %m%n\n\n\n###  To set the value for specific classes/packages, use the following format:\n## log4j.logger.<package.class>=LEVEL\n\n{0}";
+
+	public static void main(String[] args) throws IOException {
+		Log4JPropertiesGenerator propertiesGenerator = new Log4JPropertiesGenerator();
+		System.out.println("Scanning for loggers");
+		Set<String> logs = propertiesGenerator.getLogs(new File(args[0]));
+		Set<String> packages = propertiesGenerator.getPackages(logs);
+		logs.addAll(packages);
+		StringBuilder builder = new StringBuilder();
+		for (String log : logs) {
+			builder.append("#log4j.logger." + log + "=ALL").append('\n');
+		}
+		System.out.println("Writing log4j.properties");
+		FileWriter writer = new FileWriter(new File(args[1]));
+		writer.write(MessageFormat.format(TEMPLATE, builder.toString()));
+		writer.flush();
+		writer.close();
+		System.out.println("Done");
+	}
+	
+	private Set<String> getPackages(Set<String> logs) {
+		Set<String> packages = new TreeSet<String>();
+		for (String log : logs) {
+			int lastDot = log.lastIndexOf('.');
+			if (lastDot != -1) {
+				packages.add(log.substring(0, lastDot));
+			}
+		}
+		return packages;
+	}
+
+	public Set<String> getLogs(File path) throws IOException {
+		Set<String> logs = new TreeSet<String>();
+		for (File classFile : Files.getFilesRecursive(path, "class")) {
+			ClassParser parser = new ClassParser(classFile.getPath());
+			JavaClass clazz = parser.parse();
+			String log = getLog(clazz);
+			if (log != null) {
+				logs.add(log);
+			}
+		}
+		return logs;
+	}
+
+	private String getLog(JavaClass clazz) {
+		for (Field field : clazz.getFields()) {
+			if (field.isStatic() && field.getType().toString().endsWith("Log")) {
+				return clazz.getClassName();
+			}
+		}
+		return null;
+	}
+	
+}
